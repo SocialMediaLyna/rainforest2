f07e68c447550bc0d290408f2324ed2eea71b626
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-29 16:53:58 -0500

    Creating a user model, controller and view

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  # this doc created using rails g model User email:string password_digest:string (pw_digest will hash and salt)
+  has_secure_password
+  # don't forget to rake db:migrate then g controller users new create
+end
