de9edf980ce59541b00958fa8cb56ac1ade4f060
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-29 17:43:53 -0500

    Migrated reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   # this doc created using rails g model User email:string password_digest:string (pw_digest will hash and salt)
   has_secure_password
   # don't forget to rake db:migrate then g controller users new create
+  has_many :reviews
+  has_many :products, through: :reviews
 end
