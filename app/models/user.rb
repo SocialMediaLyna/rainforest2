4ce200ae2581d95d22772489b6ee71b0f654d5c5
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-30 12:45:15 -0500

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # this doc created using rails g model User email:string password_digest:string (pw_digest will hash and salt)
   has_secure_password
-  # don't forget to rake db:migrate then g controller users new create
+  validates :name, presence: true
   has_many :reviews
   has_many :products, through: :reviews
 end
