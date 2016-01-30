2ac129dd11779e828098037e3ceea7f8a1670ca2
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-30 12:38:07 -0500

    Adding authorization to ensure users are logged in before they create review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+  before_action :ensure_logged_in, only: [:create, :destroy]
+
+  def show
+    @@review = Review.find(params[:id])
+  end
+
+  def create
+    @review = @product.reviews.build(review_params)
+    @review.user = current_user
+
+    ## alt code
+#   @review = Review.new(
+#   comment: params[:review][:comment],
+#   product_id: @product.id,
+#   user_id: current_user.id
+# )
+  if @review.save
+    redirect_to products_path, notice: 'Review created successfully'
+  else
+    render 'products/show'
+  end
+end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
   resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :products do
+  resources :reviews, only: [:show, :create, :destroy]
+  end
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
